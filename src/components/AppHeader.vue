60dce9ff10a0dddb85fd89cb179541320de567fe
marcoceccarelli98 <mceccarelli98@gmail.com>
2024-06-05 11:32:54 +0200

    [UPD] Added starter components, store.js and main partials

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<template>
+  <header></header>
+</template>
+
+<style scoped></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,22 @@
+<script>
+import axios from "axios";
+import { store } from "../data/store.js";
+export default {
+  name: "AppMain",
+  components: {},
+
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {},
+  created() {},
+};
+</script>
+
+<template>
+  <main></main>
+</template>
+
+<style scoped></style>
